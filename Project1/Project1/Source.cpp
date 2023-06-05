0fcafce93520c0c1c9da4826bcf9f5e656d466f2
nastia.dovgal <nastadovgal005@gmail.com>
2023-06-05 15:58:25 +0300

    Edit function main

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int Sum(int a, int b)
+int Summ(int a, int b)
 {
 	return a = b;
 }
 
 int main()
 {
-	Sum(5, 3);
+	int a, b;
+	cin >> a >> b;
+	cout << Summ(a, b) << endl;
 	return 0;
 }
